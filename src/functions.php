bf2b876149b42a93f3bca5a81671f31caf963528
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-06-01 12:48:38 -0400

    add uuidV4

--- src/functions.php
+++ src/functions.php
@@ -156,3 +156,16 @@ function randomString(int $length): string
 
     return substr(bin2hex($randomBytes), 0, $length);
 }
+
+function uuidV4(): string
+{
+    $data = random_bytes(16);
+
+    $data[6] = chr(ord($data[6]) & 0x0F | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3F | 0x80);
+
+    return vsprintf(
+        '%s%s-%s-%s-%s-%s%s%s',
+        str_split(bin2hex($data), 4)
+    );
+}
---
+++ tests/Uuidv4FunctionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Chevere.
+ *
+ * (c) Rodolfo Berrios <rodolfo@chevere.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Chevere\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Chevere\Standard\uuidV4;
+
+final class UuidV4FunctionTest extends TestCase
+{
+    public function testFunction(): void
+    {
+        $uuidV4 = uuidV4();
+        $this->assertMatchesRegularExpression(
+            '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i',
+            $uuidV4
+        );
+    }
+}
